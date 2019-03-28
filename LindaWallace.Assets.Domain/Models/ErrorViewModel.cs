d9331989c39fa8076946de2314b02251d4e9233d
Linda Wallace <linda3nt@gmail.com>
2019-03-28 15:37:26 -0600

    Add project files.

---
+++ LindaWallace.Assets.Domain/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LindaWallace.Assets.Domain.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
